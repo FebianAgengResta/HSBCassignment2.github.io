5e8c1fbb352f96015ddcc295a60054a34c162161
FebianAgengResta <restaageng@gmail.com>
2023-09-06 07:05:03 +0700

    update

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "satu",
+    name: "Adidas",
+    price: 1399000,
+    desc: "Jumlah pesanan:",
+    img: "./images/adidas.jpg",
+  },
+  {
+    id: "dua",
+    name: "Nike",
+    price: 1099000,
+    desc: "Jumlah pembelian:",
+    img: "images/nike.jpg",
+  },
+  {
+    id: "tiga",
+    name: "Reebok",
+    price: 899000,
+    desc: "Jumlah pembelian:",
+    img: "images/reebok.jpg",
+  },
+  {
+    id: "empat",
+    name: "Skechers",
+    price: 987000,
+    desc: "Jumlah pesanan:",
+    img: "images/skechers.jpg",
+  },
+];
